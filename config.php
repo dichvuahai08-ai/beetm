969d59a1dcc408f257139f4c6c757a2fa0c96941
dichvuahai08-ai <dichvuahai08@gmail.com>
2025-08-13 16:59:37 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,1 +1,12 @@
-
+<?php
+return [
+  'BOT_TOKEN' => '7969927874:AAErxyYwZLOQCxudE6JLqGD6pAMKZf9CM0E',
+  'DB' => [
+    'host' => 'dpg-d2e5s28gjchc73e2kb40-a.oregon-postgres.render.com',
+    'port' => '5432',
+    'name' => 'beetm',
+    'user' => 'beetm_user',
+    'pass' => 'e0H83VDofaXiw1XQadZiE7WOirMPYFke'
+  ],
+  'TIMEZONE' => 'Asia/Ho_Chi_Minh'
+];
