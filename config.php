6c0cfaea4d83a59b348ef0add9a0f2d675636961
dichvuahai08-ai <dichvuahai08@gmail.com>
2025-08-13 17:27:00 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,16 +1,17 @@
 <?php
-// Thông tin Bot Telegram
-$botToken = "7969927874:AAErxyYwZLOQCxudE6JLqGD6pAMKZf9CM0E";
+return [
+  // Token bot Telegram
+  'BOT_TOKEN' => '7969927874:AAErxyYwZLOQCxudE6JLqGD6pAMKZf9CM0E',
 
-// Thông tin DB PostgreSQL
-$host = "dpg-d2e5s28gjchc73e2kb40-a.oregon-postgres.render.com";
-$port = "5432";
-$dbname = "beetm";
-$user = "beetm_user";
-$password = "e0H83VDofaXiw1XQadZiE7WOirMPYFke";
+  // Thông tin kết nối PostgreSQL
+  'DB' => [
+    'host' => 'dpg-d2e5s28gjchc73e2kb40-a.oregon-postgres.render.com', // Host từ External Database URL
+    'port' => '5432',
+    'name' => 'beetm',       // Tên database
+    'user' => 'beetm_user',  // User
+    'pass' => 'e0H83VDofaXiw1XQadZiE7WOirMPYFke'
+  ],
 
-$conn = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$password");
-if (!$conn) {
-    die("Kết nối DB thất bại: " . pg_last_error());
-}
-?>
+  // Múi giờ
+  'TIMEZONE' => 'Asia/Ho_Chi_Minh'
+];
